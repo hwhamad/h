897bf8f3b8f4fb40d1c1682bf9e64e13abd976f3
hwhamad <111175830+hwhamad@users.noreply.github.com>
2022-09-02 18:08:20 +0300

    Add files via upload

---
+++ reverseString.py
@@ -0,0 +1,16 @@
+#Create a Function that take a string as a list and reverses it
+def reverseString (string):
+    #Store the length of the string in variables
+    length = len(string)
+    newLength = length
+    #Make a list that'll store the reversed string
+    revString = []
+    #Go through every letter in string
+    for i in range(length):
+        #Take that last letter of the string and put it in revString
+        lastLetter = string.pop(newLength-1)
+        revString.append(lastLetter)
+        #Store the new length of the string
+        newLength = len(string)
+    #Return the reversed string
+    return(revString)
